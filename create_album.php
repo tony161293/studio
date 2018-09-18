c8d25f0c751ed64eb233cd9ae9f7b985691583e2
Tony Tom <ttom@perleybrook.com>
2018-09-19 02:28:11 +0530

    Changed the create album, created the error messages and added it to a same page

--- create_album.php
+++ create_album.php
@@ -1,24 +1,159 @@
 <?php include 'connect.php';?>
 <?php
+    $name = $albumtime = $location = $type = $user_id = "";
+    $error_notice = "Album Details";
+    $color_changer = "";
+    $submitflag = true;
     if(isset($_POST["album"])){
             $name = $_POST["name"];
             $albumtime = $_POST["date"];
             $location = $_POST["location"];
             $type = $_POST["type"];
             $user_id = $_POST["user_id"];
-            $password = $_POST["password"];
-            $sql = "INSERT INTO album_details(album_name, album_date, album_location, album_type, album_user, album_password, is_sorted, is_finished) VALUES ('$name', '$albumtime', '$location', '$type', '$user_id', '$password', false, false)";
-            if (mysqli_query($con, $sql)) {
-               echo '<script type="text/javascript">
-                        window.location = "/studio/upload_to_album.php"
-                     </script>';
-            } else {
-               echo "Error: " . $sql . "" . mysqli_error($conn);
+            $password = hash('sha512', $_POST["password"]);
+            if(empty($name) || empty($albumtime) || empty($location) || empty($type) || empty($user_id) || empty($password)){
+                $error_notice = "Error: Every value is required";
+                $color_changer = "red";
+                $submitflag = false;
+            }
+            if(strlen($password)<6){
+                $error_notice = "Error: At least six characters are required for the password";
+                $color_changer = "red";
+                $submitflag = false;
+            }
+            if ( preg_match('/\s/',$name) ){
+                $error_notice = "Error: The album name should not contain any space";
+                $color_changer = "red";
+                $submitflag = false;
+            }
+            $checksql = "SELECT user_id FROM album_details";
+            if($submitflag){
+                $sql = "INSERT INTO album_details(album_name, album_date, album_location, album_type, album_user, album_password, is_sorted, is_finished) VALUES ('$name', '$albumtime', '$location', '$type', '$user_id', '$password', false, false)";
+                $redirect_url = "/studio/upload_to_album.php?albumname=".$name;
+                if (mysqli_query($con, $sql)) {
+                   echo '<script type="text/javascript">
+                            window.location = "'.$redirect_url.'"
+                         </script>';
+                } else {
+                   echo "Error: " . $sql . "" . mysqli_error($conn);
+                }
             }
          }
-         else{
-            echo '<script type="text/javascript">
-                     window.location = "/studio/create_album.html"
-                  </script>';
-         }
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta charset="UTF-8">
+    <title>LightFinger</title>
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="css/main.css" rel="stylesheet" type="text/css">
+    <link href="css/view.css" rel="stylesheet" type="text/css">
+    <link href="css/datepicker.min.css" rel="stylesheet" type="text/css">
+    <link href="css/album.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="outer">
+        <div class="left_panel">
+            <div class="logo">
+                <img src="images/Logo.png">
+            </div>
+            <!-- <div class="pnel_tab">
+       <a href="view.html">
+        <section class="active">All Photos</section>
+       </a>
+       <a href="view_complete.html">
+        <section>Completed</section>
+       </a>
+    </div> -->
+        </div>
+        <div class="right_panel">
+            <div class="panel_body">
+                <div class="panel_head">
+                    <section class="tab_nav active_tab">
+                        New Album
+                    </section>
+                    <div class="clear"></div>
+                </div>
+                <div class="album_form_outer">
+                    <div class="album_wrap">
+                        <p style="color:<?php echo $color_changer; ?>"><?php echo $error_notice; ?></p>
+                        <div class="row">
+                            <form action="create_album.php" method="POST" enctype="multipart/form-data">
+                                <div class="row_block left">
+                                    <div class="block_span">
+                                        Name
+                                    </div>
+                                    <input type="text" name="name" value="<?php echo $name; ?>">
+                                </div>
+                                <div class="row_block right">
+                                    <div class="block_span">
+                                        Date
+                                    </div>
+                                    <input type='text' value="<?php echo $albumtime; ?>" name="date" class="datepicker-here" readonly data-language='en' data-position="bottom left" />
+                                </div>
+                                <div class="clear"></div>
+                        </div>
+                        <div class="row">
+                            <div class="row_block left">
+                                <div class="block_span">
+                                    Location
+                                </div>
+                                <input type="text" name="location" value="<?php echo $location; ?>">
+                            </div>
+                            <div class="row_block right">
+                                <div class="block_span">
+                                    Type
+                                </div>
+                                <input type="text" name="type" value="<?php echo $type; ?>">
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                    <div class="album_wrap">
+                        <p>User Details</p>
+                        <div class="row">
+                            <div class="row_block left">
+                                <div class="block_span">
+                                    User Id
+                                </div>
+                                <input type="text" name="user_id" value="<?php echo $user_id; ?>">
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                        <div class="row">
+                            <div class="row_block left">
+                                <div class="block_span">
+                                    Password
+                                </div>
+                                <input type="password" name="password">
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                    <section>
+                        <div>
+                            <input type="submit" value="Create Album" name="album" class="can_sub_btn">
+                        </div>
+                        </form>
+                    </section>
+                </div>
+            </div>
+        </div>
+        <script rel="script" src="js/jquery-1.8.2.min.js"></script>
+        <script rel="script" src="js/datepicker.min.js"></script>
+        <script rel="script" src="js/datepicker.en.js"></script>
+        <script rel="script" src="js/main.js"></script>
+        <script>
+        // Initialization
+        $('#my-element').datepicker([options])
+        // Access instance of plugin
+        $('#my-element').data('datepicker')
+        </script>
+</body>
+
+</html>
