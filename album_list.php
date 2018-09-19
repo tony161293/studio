7030ceb351c486cf0f44b584628bed8ad651dee9
Gokul <gokul@perleybrook.com>
2018-09-19 22:11:44 +0530

    create sample login page and implemented login

--- album_list.php
+++ album_list.php
@@ -1,24 +1,4 @@
 <?php include 'connect.php';?>
-<?php
-    //  if ($nonSortedList->num_rows > 0) {
-    //     // output data of each row
-    //     while($row = $result->fetch_assoc()) {
-    //         echo "id: " . $row["album_name"]. " - Name: " . $row["album_type"]. " " . $row["id"]. "<br>";
-    //     }
-    // } else {
-    //     echo "0 results";
-    // }
-    // $con->close();
-    //  if (mysqli_query($con, $sql)) {
-    //      echo $sql;
-    //     // echo '<script type="text/javascript">
-    //     //          window.location = "/studio/upload_to_album.php"
-    //     //       </script>';
-    //  } else {
-
-    //     echo "Error: " . $sql . "" . mysqli_error($conn);
-    //  }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -70,16 +50,16 @@
         <div class="panel_body">
             <div class="common_panel panel_all" id="all_photos">
                 <?php
-                    $sql = "SELECT * FROM album_details WHERE is_sorted=true AND is_finished=false";
-                    $result = $con->query($sql);
-                    if ($result->num_rows > 0) {
-                        while($row = $result->fetch_assoc()) {
-                            echo "<div class=\"panel_blocks\">" . $row["album_name"] . "</div>";                        
-                        }
-                    } 
-                ?>
+$sql = "SELECT * FROM album_details WHERE is_sorted=true AND is_finished=false";
+$result = $con->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<div class=\"panel_blocks\">" . $row["album_name"] . "</div>";
+    }
+}
+?>
             </div>
-            
+
     </div>
 
 </div>
